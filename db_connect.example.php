f20efe7e146c57c3a201faa98dfb63268c25b7ac
unknown <mario.lara.martin@gmail.com>
2025-10-31 12:01:21 -0500

    Remove db_connect.php and add .gitignore

---
+++ db_connect.example.php
@@ -0,0 +1,11 @@
+<?php
+// COPIA ESTE ARCHIVO COMO .env Y LLENA TUS DATOS
+
+// Ejemplo:
+// DB_HOST=localhost
+// DB_USER=root
+// DB_PASS=
+// DB_NAME=mi_sistema
+
+die("Crea un archivo .env con tus credenciales");
+?>
